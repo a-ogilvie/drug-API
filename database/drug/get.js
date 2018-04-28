5105da147168e40e1f208af2d38c108452692c2f
a-ogilvie <alexander.ogilvie@icloud.com>
2018-04-28 20:55:00 +0900

    Rewrite drug.get

--- database/drug/get.js
+++ database/drug/get.js
@@ -1,21 +1,9 @@
-const Promise = require("bluebird");
-
 module.exports = function(knex, Drug) {
-  return (params) => {
-    const name = params.name;
-
-    return Promise.try(() => {
-      if (!name) throw new Error("Drug name must be provided.");
-    })
-      .then(() => {
-        return knex("drug")
-          .where({ name: name.toLowerCase() })
-          .select();
-      })
-      .then((dbDrug) => {
-        if (dbDrug.length) return new Drug(dbDrug.pop());
-
-        throw new Error("Drug not found!");
+  return () => {
+    return knex("drug")
+      .select("*")
+      .then((dbDrugs) => {
+        return dbDrugs.map((dbDrug) => new Drug(dbDrug));
       });
   };
 };

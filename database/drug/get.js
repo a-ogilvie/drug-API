c2e14164bb89625966240550fa85d250a182868f
a-ogilvie <alexander.ogilvie@icloud.com>
2018-04-28 13:48:13 +0900

    Implement db.drug.get

--- database/drug/get.js
+++ database/drug/get.js
@@ -1,7 +1,21 @@
+const Promise = require("bluebird");
+
 module.exports = function(knex, Drug) {
   return (params) => {
     const name = params.name;
 
-    return knex("drug").select();
+    return Promise.try(() => {
+      if (!name) throw new Error("Drug name must be provided.");
+    })
+      .then(() => {
+        return knex("drug")
+          .where({ name: name.toLowerCase() })
+          .select();
+      })
+      .then((dbDrug) => {
+        if (dbDrug.length) return new Drug(dbDrug.pop());
+
+        throw new Error("Drug not found!");
+      });
   };
 };
--- database/drug/index.js
+++ database/drug/index.js
@@ -1,5 +1,12 @@
 class Drug {
-  constructor(dbDrug) {}
+  constructor(dbDrug) {
+    this.id = dbDrug.id;
+    this.name = dbDrug.name;
+    this.type = dbDrug.drug_type;
+    this.description = dbDrug.description;
+    this.price = dbDrug.price;
+    this.stock = dbDrug.stock;
+  }
 }
 
 module.exports = function(knex) {

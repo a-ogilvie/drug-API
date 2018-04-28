8a8f966f8de74c57b2fe364a8fb82719b2549f23
a-ogilvie <alexander.ogilvie@icloud.com>
2018-04-28 20:54:38 +0900

    Rewrite test for drug.get and write test for drug.post

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const { expect } = require("chai");
 const config = require("../config");
+config.database.connection.database = "drug_test";
 const knex = require("knex")(config.database);
 const database = require("../database")(config.database);
 
@@ -9,24 +10,70 @@ const forcePromiseReject = function() {
 
 describe("drug", () => {
   describe("#get", () => {
+    before(() => {
+      newDrug = {
+        name: "aspirin",
+        type: "small molecule",
+        description: "another painkiller",
+        price: "0.75",
+        stock: 3
+      };
+
+      return database.drug.post(newDrug);
+    });
+
+    after(() => knex("drug").del());
+
+    it("returns a list of all drugs available", () => {
+      database.drug.get().then((drugs) => {
+        expect(drugs).to.be.an("array");
+        expect(drugs[0].id).to.be.a("number");
+      });
+    });
+  });
+
+  describe("#post", () => {
+    let newDrug;
     context("when bad params are given", () => {
       it("politely refuses", () => {
         database.drug
-          .get({})
+          .post({})
           .then(forcePromiseReject)
           .catch((err) => {
-            expect(err.message).to.equal("Drug name must be provided.");
+            expect(err.message).to.equal("Must provide name!");
           });
       });
     });
 
     context("when good params are given", () => {
-      it("retrieves the correct info", () => {
-        const params = { name: "paracetamol" };
-        database.drug.get(params).then((drugObj) => {
-          expect(drugObj.name).to.equal(params.name);
-          expect(drugObj.id).to.be.a("number");
+      before(() => {
+        newDrug = {
+          name: "paracetamol",
+          type: "small molecule",
+          description: "common painkiller",
+          price: "0.75",
+          stock: 3
+        };
+      });
+
+      after(() => knex("drug").del());
+
+      it("adds to the database", () => {
+        return database.drug.post(newDrug).then((drug) => {
+          expect(drug).to.include(newDrug);
+          expect(drug.id).to.be.a("number");
         });
+      });
+
+      it("rejects duplicate entries", () => {
+        database.drug
+          .post(newDrug)
+          .then(forcePromiseReject)
+          .catch((err) => {
+            expect(err.message).to.equal(
+              "That drug already exists in the database."
+            );
+          });
       });
     });
   });

7365bd5b67fb6c47ca78a8ae4368b8dcaed366a1
a-ogilvie <alexander.ogilvie@icloud.com>
2018-04-28 20:53:40 +0900

    Add body-parser

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const database = require("./database")(config.database);
 
 const apiRouter = require("./routes/api")(database);
 
+const bodyParser = require("body-parser");
+
 const express = require("express");
 const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, authorization"
+  );
+  next();
+});
+
+app.use(bodyParser.json({ type: "application/json", limit: "50mb" }));
 
 app.use("/api", apiRouter);
 

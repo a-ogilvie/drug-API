892f2a66fc17483123736ecd8421a3e5c850f3cd
a-ogilvie <alexander.ogilvie@icloud.com>
2018-04-29 14:29:13 +0900

    Delete unnecessary IDs

--- views/apiInfo.js
+++ views/apiInfo.js
@@ -8,17 +8,14 @@ module.exports = [
     body: "Use this to get info on one specific drug."
   },
   {
-    id: 3,
     name: "POST api/drug/",
     body: "Use this to add one drug to the database."
   },
   {
-    id: 4,
     name: "PATCH api/drug/{drug name}",
     body: "Use this to alter the properties of one drug."
   },
   {
-    id: 4,
     name: "DELETE api/drug/{drug name}",
     body: "Use this to delete one drug from the database."
   }

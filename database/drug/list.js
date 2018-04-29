d9c75eb7a010e29f510d08491daa0899fc5067fb
a-ogilvie <alexander.ogilvie@icloud.com>
2018-04-29 10:43:02 +0900

    Add alpha ordering to db.drub.list

--- database/drug/list.js
+++ database/drug/list.js
@@ -2,6 +2,7 @@ module.exports = function(knex, Drug) {
   return () => {
     return knex("drug")
       .select("*")
+      .orderBy("name")
       .then((dbDrugs) => {
         return dbDrugs.map((dbDrug) => new Drug(dbDrug));
       });

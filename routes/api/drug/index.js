e23b255943cab3ab6db2d7f7782863dc2ec75220
a-ogilvie <alexander.ogilvie@icloud.com>
2018-04-29 22:56:49 +0900

    Add error handling for server errors

--- routes/api/drug/index.js
+++ routes/api/drug/index.js
@@ -7,17 +7,27 @@ const CREATED = 201;
 const BAD_REQ = 400;
 const NOT_FOUND = 404;
 const CONFLICT = 409;
+const SERVER_ERROR = 500;
 
 module.exports = function(database) {
   router.get("/", (req, res) => {
-    database.drug.list().then((drugs) => res.status(OK).json(drugs));
+    database.drug
+      .list()
+      .then((drugs) => res.status(OK).json(drugs))
+      .catch((err) => res.status(SERVER_ERROR).send(err.message));
   });
 
   router.get("/:name", (req, res) => {
     database.drug
       .get(req.params)
       .then((drug) => res.status(OK).json(drug))
-      .catch((err) => res.status(BAD_REQ).send(err.message));
+      .catch((err) => {
+        if (err.message === "Drug not found!") {
+          return res.status(BAD_REQ).send(err.message);
+        } else {
+          return res.status(SERVER_ERROR).send(err.message);
+        }
+      });
   });
 
   router.patch("/:name", (req, res) => {
@@ -34,14 +44,28 @@ module.exports = function(database) {
         return database.drug.patch(newInfo);
       })
       .then((drug) => res.status(OK).json(drug))
-      .catch((err) => res.status(NOT_FOUND).send(err.message));
+      .catch((err) => {
+        if (err.message === "Drug not found!") {
+          return res.status(NOT_FOUND).send(err.message);
+        } else if (err.message === "Must provide name!") {
+          return res.status(BAD_REQ).send(err.message);
+        } else {
+          return res.status(SERVER_ERROR).send(err.message);
+        }
+      });
   });
 
   router.delete("/:name", (req, res) => {
     database.drug
       .delete(req.params)
       .then(() => res.status(OK).send("Drug deleted."))
-      .catch((err) => res.status(NOT_FOUND).send(err.message));
+      .catch((err) => {
+        if (err.message === "Drug not found!") {
+          return res.status(NOT_FOUND).send(err.message);
+        } else {
+          return res.status(SERVER_ERROR).send(err.message);
+        }
+      });
   });
 
   router.post("/", (req, res) => {
@@ -53,7 +77,17 @@ module.exports = function(database) {
         stock: req.body.stock
       })
       .then((drug) => res.status(CREATED).json(drug))
-      .catch((err) => res.status(CONFLICT).send(err.message));
+      .catch((err) => {
+        if (err.message.match(/Must provide/)) {
+          return res.status(BAD_REQ).send(err.message);
+        } else if (
+          err.message === "That drug already exists in the database."
+        ) {
+          return res.status(CONFLICT).send(err.message);
+        } else {
+          return res.status(SERVER_ERROR).send(err.message);
+        }
+      });
   });
 
   return router;
